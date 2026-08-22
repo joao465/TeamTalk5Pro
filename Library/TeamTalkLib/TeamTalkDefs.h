d8ec0135ebac8c552c013c5b62ed8849f709a8d0
joao465 <128336528+joao465@users.noreply.github.com>
2026-08-22 13:57:30 -0300

    Fix TeamTalk SDK DLL version metadata

--- Library/TeamTalkLib/TeamTalkDefs.h
+++ Library/TeamTalkLib/TeamTalkDefs.h
@@ -24,10 +24,10 @@
 #if !defined(TEAMTALKDEFS_H)
 #define TEAMTALKDEFS_H
 
-#define TEAMTALK_VERSION            "5.23.0.5202"
-#define TEAMTALK_RC_VERSION         5,23,0,5202
-#define TEAMTALK_RC_FILEVERSION     "5, 23, 0, 5202"
-#define TEAMTALK_VERSION_POSTFIX    " - Unreleased"
+#define TEAMTALK_VERSION            "5.26.2.5202"
+#define TEAMTALK_RC_VERSION         5,26,2,5202
+#define TEAMTALK_RC_FILEVERSION     "5, 26, 2, 5202"
+#define TEAMTALK_VERSION_POSTFIX    ""
 
 #if defined(ENABLE_TEAMTALKPRO)
 #define TEAMTALK_LIB_NAME "TeamTalk5Pro"
